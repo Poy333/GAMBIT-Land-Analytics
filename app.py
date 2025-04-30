6896e122f5faa6deb5c11475a6b72faa2902497c
Poy333 <felquezmagalona27@gmail.com>
2025-04-30 21:06:38 +0800

    working

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template  # type: ignore
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/programs')
+def programs():
+    return render_template('programs.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/soil_science')
+def soil_science():
+    return render_template('soil_science.html')
+
+# Add the new route for land_mapping
+@app.route('/land_mapping')
+def land_mapping():
+    return render_template('land_mapping.html')  # Create this template
+
+if __name__ == '__main__':
+    app.run(debug=True)
